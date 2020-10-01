59d8da7390cc98f1faf4e54eb4c661af6600ff77
James Payne <james.payne1349@gmail.com>
2020-10-01 17:56:45 +0800

    autofocus added to login, new account box added

--- app/static/scripts/loginScripts.js
+++ app/static/scripts/loginScripts.js
@@ -1,15 +1,5 @@
 // @ts-check
 
 
-// #login_content name of main page div..
-let page = $('#login_content');
+// login_content name of main page div..
 
-
-function createLoginTitle() {
-
-    let title_div = document.createElement('div');
-    title_div.className = 'title_div';
-    title_div.innerText = 'Madd Scheduling';
-
-    page[0].appendChild(title_div);
-}
